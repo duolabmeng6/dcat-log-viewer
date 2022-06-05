965c09d59d2ff727feb9cc2e723d0c9375e31e49
duolabmeng <duolabmeng@qq.com>
2022-06-05 17:02:41 +0800

    first commit

---
+++ src/LogViewerServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Dcat\Admin\LogViewer;
+
+use Dcat\Admin\Extend\ServiceProvider;
+
+class LogViewerServiceProvider extends ServiceProvider
+{
+	protected $js = [
+//        'js/index.js',
+    ];
+	protected $css = [
+//		'css/index.css',
+	];
+
+    // 定义菜单
+    protected $menu = [
+        [
+            'title' => '日志查看',
+            'uri'   => 'auth/log-viewer',
+            'icon'  => 'fa-database',
+        ]
+    ];
+
+	public function register()
+	{
+		//
+	}
+
+	public function init()
+	{
+		parent::init();
+
+		//
+
+	}
+
+	public function settingForm()
+	{
+		return new Setting($this);
+	}
+}
